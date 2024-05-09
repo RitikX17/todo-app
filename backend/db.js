e1681adaee9cd493cf4b397faefae2db93ce68cb
Ritik Thak <rutikthak17@gmail.com>
2024-05-09 16:54:19 +0530

    added db.js

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { boolean } from "zod";
+
+mongoose.connect("mongodb+srv://rutikthak17:Ritik@123@todoapp.bded8ux.mongodb.net/todos")
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+const todo = mongoose.model('todos', todoSchema);
+module.exports = {
+    todo
+}
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
 import express from 'express';
+import {createTodo,updateTodo} from './types';
 const app = express();
 
 app.use(express.json());
 
 app.post("todo",(req,res)=>{
-
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayLoad);
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
+    //put it in mongoFb
 })
 
 app.get("/todos",(req,res)=>{
     
 })
 
-app.put("/completed",(req,res)=>[
-
-])
+app.put("/completed",(req,res)=>{
+    const updatePayload = req.body;
+    const parsedPayLoad = updateTodo.safeParse(updatePayload);
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
+})
