cb9787cdd42170a23b6f11eaa28bbb6d9113fc87
Ritik Thak <rutikthak17@gmail.com>
2024-08-20 21:22:00 +0530

    mid commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {createTodo,updateTodo} from './types.js';
 import todo from './db.js';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/todo", async (req,res)=>{
     const createPayLoad = req.body;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import './App.css';
 import { CreateTodo } from './components/CreateTodo';
 import { Todos } from './components/Todos';
 function App() {
+  const [todos,setTodos] = useState([]);
   return (
     <>
       <CreateTodo></CreateTodo>
